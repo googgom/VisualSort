00f9f173b30ec1c6fdde48f113ddc9bfee15c54e
Matvey Volodchenko <41430895+googgom@users.noreply.github.com>
2024-07-06 17:27:48 +0300

    Ver-1.0

--- src/Merge.java
+++ src/Merge.java
@@ -1,17 +1,40 @@
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Queue;
 
 public class Merge {
-    private static int[] Array;
+    private static int[] Arrayv;
     private static int[] Current;
-    private static Queue<Boolean> OrderQueue;//MergeQueue now?
-    private static Queue<int[]> MergeQueue;
-    private static Queue<int[]> MergeCallQueue;
+    private static ArrayList<String> Stats_12;
+    private static ArrayList<int[]> Arrays_12;
 
+    public ArrayList<String> getStats( ){
+        return Stats_12;
+    }
 
-    private static int[] merge(int[] a, int[] l, int[] r, int left, int right) {
-        OrderQueue.add(false);
-        MergeCallQueue.add(a);
+    public ArrayList<int[]> getArrays( ){
+        return Arrays_12;
+    }
+
+    private static void merge(int[] a, int[] l, int[] r, int left, int right) {
+
+        //if( l == null ) l = new int[0];
+        //if( r == null ) r = new int[0];
+        String temp = "Merge sort: Merging: ";
+        temp += Arrays.toString(l);
+        temp += " and: ";
+        temp += Arrays.toString(r);
+        Stats_12.add("Step-" + Stats_12.size() / 1 + " " +temp );
+        int[] temp3 = new int[l.length+r.length];
+        int temp4 = 0;
+        for( ; temp4 < l.length; ++ temp4 ){
+            temp3[temp4] = l[temp4];
+        }
+        for( ; temp4 < l.length + r.length; ++ temp4 ){
+            temp3[temp4] = r[temp4 - l.length];
+        }
+        Arrays_12.add(temp3);
 
         int i = 0, j = 0, k = 0;
         while (i < left && j < right) {
@@ -29,16 +52,23 @@ public class Merge {
             a[k++] = r[j++];
         }
 
-        OrderQueue.add(false);
-        MergeCallQueue.add(a);
-        return a;
+        temp = "Merge Sort: Merged into: ";
+        temp += Arrays.toString(a);
+        Stats_12.add("Step-" + Stats_12.size() / 1 + " " + temp );
+        int [] temp5 = new int[a.length];
+        for( temp4 = 0; temp4 < a.length; ++ temp4 ) temp5[temp4] = a[temp4];
+        Arrays_12.add(temp5);
+        return;
     }
 
-    private static int[] mergeSort(int[] a) {
-        //a = MergeQueue.remove();
+    private static void mergeSort(int[] a) {
+        Stats_12.add("Step-" + Stats_12.size() / 1 + " Merge sort: Dividing: " + Arrays.toString(a));
+        int [] temp7 = new int[a.length];
+        for( int i = 0; i < temp7.length; ++ i ) temp7[i] = a[i];
+        Arrays_12.add(temp7);
         int n = a.length;
         if (n < 2) {
-            return null;
+            return;
         }
         int mid = n / 2;
         int[] l = new int[mid];
@@ -50,56 +80,49 @@ public class Merge {
         for (int i = mid; i < n; i++) {
             r[i - mid] = a[i];
         }
-        MergeQueue.add(l);
-        OrderQueue.add(true);
-        l = mergeSort(l);
-        MergeQueue.add(l);
-        OrderQueue.add(true);
 
-        MergeQueue.add(r);
-        OrderQueue.add(true);
-        r = mergeSort(r);
-        MergeQueue.add(r);
-        OrderQueue.add(true);
+        Stats_12.add("Step-" + Stats_12.size() / 1 + " Merge sort: Divided: " + Arrays.toString(a)+" into " + Arrays.toString(l) + " and " + Arrays.toString(r));
+        int [] temp8 = new int[a.length];
+        for( int i = 0; i < temp8.length; ++ i ) temp8[i] = a[i];
+        Arrays_12.add(temp8);
+        mergeSort(l);
+        mergeSort(r);
 
-        return merge(a, l, r, mid, n - mid);
+        merge(a, l, r, mid, n - mid);
     }
 
-    private int[] sort(){
-        return mergeSort( Array );
+    private void launch(){
+        System.out.println("Merge Sort Launched");
+        //System.out.println(Arrays.toString(mergeSort( Array )));
+        Stats_12.add("Step-" + Stats_12.size() / 1 + " Array received: " + Arrays.toString(Arrayv));
+        int [] temp8 = new int[Arrayv.length];
+        for( int i = 0; i < temp8.length; ++ i ) temp8[i] = Arrayv[i];
+        Arrays_12.add(temp8);
+        mergeSort(Arrayv);
+        Stats_12.add("Step-" + Stats_12.size() / 1 + " Array sorted: " + Arrays.toString(Arrayv));
+        int [] temp7 = new int[Arrayv.length];
+        for( int i = 0; i < temp7.length; ++ i ) temp7[i] = Arrayv[i];
+        Arrays_12.add(temp7);
     }//Нельзя
-
-    private Boolean sortStep( ){
-        if(OrderQueue.isEmpty()) return false;
-        if(OrderQueue.remove()){
-            Current = MergeQueue.remove();
-        }else{
-            Current = MergeCallQueue.remove();
-        }
-        return true;
-    }//Нельзя
-
-    public Boolean nextStep( ){
-        return sortStep( );
-    }//Можно целиком использовать в других классах
 
     public int[] getCurrent( ){
         return Current;
     }//Можно
 
     public void clear( ){
-        MergeQueue.clear();
-        OrderQueue.clear();
-        MergeCallQueue.clear();
-        Array = null;
-        Current = null;
+        if( Arrays_12!=null ) Arrays_12.clear();
+        if( Stats_12!=null ) Stats_12.clear();
+        if(Arrayv != null) Arrayv = null;
+        if(Current != null)Current = null;
     }//Нельзя
 
     public void load( int[] LoadArray ){
         clear();
-        Array = new int[LoadArray.length];
-        System.arraycopy(LoadArray, 0, Array, 0, Array.length);
-        sort( );
+        Arrayv = new int[LoadArray.length];
+        Stats_12 = new ArrayList<>();
+        Arrays_12 = new ArrayList<>();
+        System.arraycopy(LoadArray, 0, Arrayv, 0, Arrayv.length);
+        launch( );
     }//Можно целиком использовать в других классах
 
 }
--- src/Visual.java
+++ src/Visual.java
@@ -1,13 +1,50 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.IntStream;
+
 
 public class Visual {
 
-    private static String currentMode = "Merge";
+    private static String currentMode = "null";
     private static Boolean restartAsked = true;
+    static int n = 0;
+    static int index = 0;
+    static int[] A_bckp = new int[22];
+    static int[] A = new int[22];
+    static int[] B = new int[22];
+    private static int curW = 600;
+    private static int curH = 400;
+    private static ArrayList<String> Stats_12;
+    private static ArrayList<int[]> Arrays_12;
+    public static Bubble handlerB = null;
+    public static Merge handlerM = null;
+    public static Insertion handlerI = null;
 
     private static void sortCall( ){
 
+    }
+
+    private static int[] shrinken( int [] AE ){
+        int[] temp9 = new int[AE.length];///сжимает массив для графика......
+        int[] temp10 = new int[AE.length];
+        for( int i = 0; i < AE.length; ++ i ){
+            temp10[i] = AE[i];
+        }
+        IntStream sorted1 = Arrays.stream(temp10).sorted();
+        temp10 = sorted1.toArray();
+        HashMap<Integer, Integer> PTSD = new HashMap<>();
+        for( int i = 0; i < AE.length; ++ i ){
+            PTSD.put(temp10[i],i);
+        }
+        for( int i = 0; i < AE.length; ++ i ){
+            temp9[i] = PTSD.get(AE[i]);
+        }
+        return temp9;
     }
 
 
@@ -16,53 +53,359 @@ public class Visual {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //^ стандартное поведение при закрытии окна - завершение приложения
         frame.setSize(600, 400); // размеры окна
-        frame.setResizable(false);
+        frame.setMinimumSize(new Dimension(600, 400)); // размеры окна
+        frame.setPreferredSize(new Dimension(600, 400)); // размеры окна
+        //frame.setResizable(false);
         frame.setLocationRelativeTo(null); // окно - в центре экрана
         JButton button = new JButton("Press"); // Экземпляр класса JButton
-        frame.getContentPane().add(button); // Добавляем кнопку на Frame
+        //frame.getContentPane().add(button); // Добавляем кнопку на Frame
         //^ getContentPane() - клиентская область окна
+        frame.addComponentListener(new ComponentListener() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                System.out.println("Window - Resized");
+                System.out.println("Current screen size: " + frame.getWidth() + "w " + frame.getHeight() + "h");
+                curW = frame.getWidth();
+                curH = frame.getHeight();
+            }
 
+            @Override
+            public void componentMoved(ComponentEvent e) {
+                //System.out.println("Window - Moved");
+            }
+
+            @Override
+            public void componentShown(ComponentEvent e) {
+                //System.out.println("Window - Shown");
+            }
+
+            @Override
+            public void componentHidden(ComponentEvent e) {
+                //System.out.println("Window - Hidden");
+            }
+        });
 
         JMenuBar menuBar = new JMenuBar( );
         JMenu mode = new JMenu("Mode");
+        JMenu file = new JMenu("File");
 
 
+        JMenuItem openFile = new JMenuItem("Open");
         JMenuItem bubbleMode = new JMenuItem("Bubble Mode");
         JMenuItem mergeMode = new JMenuItem("Merge Mode");
         JMenuItem insertionMode = new JMenuItem("Insertion Mode");
-        JMenuItem restart = new JMenuItem("Restart");
-        restart.addActionListener(new ActionListener() {
+        JButton previous = new JButton("Previous");
+        JButton next = new JButton("Next");
+
+
+
+
+
+        DefaultTableModel model = new DefaultTableModel();
+        JTable table = new JTable(model);
+        model.addColumn(A);
+        table.setMaximumSize(new Dimension(400, 180));
+        table.setBackground(Color.PINK);
+
+
+        DefaultListModel dlm = new DefaultListModel();
+        JList list = new JList(dlm);
+        list.setMaximumSize(new Dimension(300, 180));
+        //list.setVisibleRowCount(1);
+        //list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        list.setBackground(Color.green);
+        if( A != null ) {
+            dlm.clear();
+            for(Integer word : Arrays.stream(A).toArray()) {
+                dlm.addElement(word);
+            }
+        }
+
+
+        DefaultListModel dlm2 = new DefaultListModel();
+        JList list2 = new JList(dlm2);
+        list2.setMaximumSize(new Dimension(300, 180));
+        //list2.setVisibleRowCount(1);
+        //list2.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        list2.setBackground(Color.red);
+        if( B != null ) {
+            dlm2.clear();
+            for(Integer word : Arrays.stream(B).toArray()) {
+                dlm2.addElement(word);
+            }
+        }
+
+        JTextArea textLeft = new JTextArea("Open File -> Select Mode\nUse Previous and Next buttons for navigation");
+        //textLeft.setMaximumSize(new Dimension(160,90));
+        textLeft.setEditable(false);
+        JTextArea textRight = new JTextArea("Open File -> Select Mode\nUse Previous and Next buttons for navigation");
+        //textRight.setMinimumSize(new Dimension(160,90));
+        textRight.setEditable(false);
+
+        final int WI = 8, HI = 11-2;
+        final int WO = 12, HO = 12-3;
+
+        Canvas LG = new Canvas(){
+            public void paint(Graphics g) {
+                setBackground(Color.lightGray);
+                if(Objects.equals(currentMode, "Merge")) setForeground(Color.red);
+                if(Objects.equals(currentMode, "null")) setForeground(Color.darkGray);
+                if(Objects.equals(currentMode, "Bubble")) setForeground(Color.blue);
+                if(Objects.equals(currentMode, "Insertion")) setForeground(Color.green);
+
+                if( Arrays_12 == null ) return;
+                int [] grp = shrinken(Arrays_12.get(index));
+                int x = this.getWidth(), y = 5;
+                x -= WI;
+                for( int i = 0; i < grp.length; ++ i ){
+                    x = this.getWidth();
+                    x -= WI;
+                    for( int j = 0; j <= grp[i]; ++ j ){
+                        g.fillRect(x, y, WI, HI);
+                        x -= WI;
+                    }
+                    y += HI;
+                    y += HO;
+                }
+            }
+        };
+
+        Canvas RG = new Canvas(){
+            public void paint(Graphics g) {
+                setBackground(Color.lightGray);
+                if(Objects.equals(currentMode, "Merge")) setForeground(Color.red);
+                if(Objects.equals(currentMode, "null")) setForeground(Color.darkGray);
+                if(Objects.equals(currentMode, "Bubble")) setForeground(Color.blue);
+                if(Objects.equals(currentMode, "Insertion")) setForeground(Color.green);
+
+
+                if( Arrays_12 == null ) return;
+                int [] grp = shrinken(Arrays_12.get(index+1));
+                int x = this.getWidth(), y = 5;
+                x -= WI;
+                for( int i = 0; i < grp.length; ++ i ){
+                    x = this.getWidth();
+                    x -= WI;
+                    for( int j = 0; j <= grp[i]; ++ j ){
+                        g.fillRect(x, y, WI, HI);
+                        x -= WI;
+                    }
+                    y += HI;
+                    y += HO;
+                }
+            }
+        };
+
+
+
+
+        JSplitPane splitLG = new JSplitPane( SwingConstants.VERTICAL, LG, list );
+        splitLG.getLeftComponent().setMinimumSize(new Dimension(160,200));
+        JSplitPane splitLeft = new JSplitPane( SwingConstants.HORIZONTAL, textLeft, splitLG );
+        splitLeft.setEnabled(false);
+
+        JSplitPane splitRG = new JSplitPane( SwingConstants.VERTICAL, RG, list2 );
+        splitRG.getLeftComponent().setMinimumSize(new Dimension(160,200));
+        JSplitPane splitRight = new JSplitPane( SwingConstants.HORIZONTAL, textRight, splitRG );
+        splitRight.setEnabled(false);
+
+        JSplitPane splitPane = new JSplitPane( SwingConstants.VERTICAL, splitLeft, splitRight );
+        splitPane.setBackground(Color.gray);
+        //splitPane.setDividerLocation(0.5);
+        splitPane.getLeftComponent().setMinimumSize(new Dimension(280,400));
+        splitPane.getRightComponent().setMinimumSize(new Dimension(280,400));
+
+
+
+
+        openFile.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                restartAsked = true;
+                //restartAsked = true;
+                System.out.println("Open File Button - Clicked");
+                currentMode = "null";
+
+
+                FileDialog dialog = new FileDialog((Frame)null, "Select File to Open");
+                dialog.setMode(FileDialog.LOAD);
+                dialog.setVisible(true);
+                String filename = dialog.getFile();
+                dialog.dispose();
+                System.out.println(filename + " chosen.");
+
+                if( filename != null ) {
+                    try {
+                        File Obj = new File(filename);
+                        Scanner Reader = new Scanner(Obj);
+                        n = Reader.nextInt();
+                        System.out.println("Array size: " + n);
+                        A = new int[n];
+                        for( int i = 0; i < n; ++ i ){
+                            A[i] = Reader.nextInt();
+                        }
+                        System.out.println(Arrays.toString(A));
+                        if( A != null ) {
+                            dlm.clear();
+                            for(Integer word : Arrays.stream(A).toArray()) {
+                                dlm.addElement(word);
+                            }
+                        }
+                        A_bckp = A;
+                        Reader.close();
+                    } catch (FileNotFoundException er) {
+                        System.out.println("An error - File Open - occurred.");
+                    }
+                }
+
+
+            }
+        });
+        previous.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //restartAsked = true;
+                System.out.println("Previous Button - Clicked");
+                if( currentMode == "null" ){
+                    textLeft.setText("Select Sort Mode.\nMenu:Mode:[Any]");
+                    return;
+                }
+                String temp123 = textRight.getText();
+                if( temp123.equals("Press Next.\nTo Continue") ) return;
+                index--;
+
+                if( index < 0 ) index++;
+                if(1==1)
+                {
+                    textLeft.setText(Arrays.toString(Arrays_12.get(index)) + "\n" + Stats_12.get(index));
+                    textRight.setText(Arrays.toString(Arrays_12.get(index+1)) + "\n" + Stats_12.get(index+1));
+                    if( Arrays_12.get(index) != null ) {
+                        dlm.clear();
+                        for(Integer word : Arrays.stream(Arrays_12.get(index)).toArray()) {
+                            dlm.addElement(word);
+                        }
+                    }
+                    if( Arrays_12.get(index+1) != null ) {
+                        dlm2.clear();
+                        for(Integer word : Arrays.stream(Arrays_12.get(index+1)).toArray()) {
+                            dlm2.addElement(word);
+                        }
+                    }
+                    //index--;
+                    //index--;
+                }
+                RG.repaint();
+                LG.repaint();
+            }
+        });
+        next.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //restartAsked = true;
+                System.out.println("Next Button - Clicked");
+                if( currentMode == "null" ) {
+                    textLeft.setText("Select sort.\nMenu:Mode");
+                    return;
+                }
+                index++;
+                String temp123 = textRight.getText();
+                if( temp123.equals("Press Next.\nTo Continue") ) index = 0; else
+                if( index + 1 >= Arrays_12.size( ) ) index --;
+                if(1==1)
+                {
+                    textLeft.setText(Arrays.toString(Arrays_12.get(index)) + "\n" + Stats_12.get(index));
+                    textRight.setText(Arrays.toString(Arrays_12.get(index+1)) + "\n" + Stats_12.get(index+1));
+                    if( Arrays_12.get(index) != null ) {
+                        dlm.clear();
+                        for(Integer word : Arrays.stream(Arrays_12.get(index)).toArray()) {
+                            dlm.addElement(word);
+                        }
+                    }
+                    if( Arrays_12.get(index+1) != null ) {
+                        dlm2.clear();
+                        for(Integer word : Arrays.stream(Arrays_12.get(index+1)).toArray()) {
+                            dlm2.addElement(word);
+                        }
+                    }
+                    //index++;
+                }
+                RG.repaint();
+                LG.repaint();
             }
         });
         mergeMode.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 currentMode = "Merge";
+                System.out.println("Merge Button - Clicked");
+                handlerM = new Merge();
+                //handler.load( int[1,2] );
+                A = A_bckp;
+                handlerM.load(A);
+                Stats_12 = handlerM.getStats();
+                Arrays_12 = handlerM.getArrays();
+                index = 0;
+                for( int i = 0; i < Stats_12.size(); ++i ) {
+                    System.out.println(Stats_12.get(i) + " -- " + Arrays.toString(Arrays_12.get(i)));
+                }
+                textRight.setText("Press Next.\nTo Continue");
+                RG.repaint();
+                LG.repaint();
             }
         });
         bubbleMode.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 currentMode = "Bubble";
+                System.out.println("Bubble Button - Clicked");
+                handlerB = new Bubble();
+                //handler.load( int[1,2] );
+                A = A_bckp;
+                handlerB.load(A);
+                Stats_12 = handlerB.getStats();
+                Arrays_12 = handlerB.getArrays();
+                index = 0;
+                for( int i = 0; i < Stats_12.size(); ++i ) {
+                    System.out.println(Stats_12.get(i) + " -- " + Arrays.toString(Arrays_12.get(i)));
+                }
+                textRight.setText("Press Next.\nTo Continue");
+                RG.repaint();
+                LG.repaint();
             }
         });
         insertionMode.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 currentMode = "Insertion";
+                System.out.println("Insertion Button - Clicked");
+                handlerI = new Insertion();
+                //handler.load( int[1,2] );
+                A = A_bckp;
+                handlerI.load(A);
+                Stats_12 = handlerI.getStats();
+                Arrays_12 = handlerI.getArrays();
+                index = 0;
+                for( int i = 0; i < Stats_12.size(); ++i ) {
+                    System.out.println(Stats_12.get(i) + " -- " + Arrays.toString(Arrays_12.get(i)));
+                }
+                textRight.setText("Press Next.\nTo Continue");
+                RG.repaint();
+                LG.repaint();
             }
         });
 
 
+
+
+
+        file.add( openFile );
         mode.add( bubbleMode );
         mode.add( mergeMode );
         mode.add( insertionMode );
 
 
 
+        menuBar.add(file);
         menuBar.add(mode);
-        menuBar.add(restart);
+        menuBar.add(previous);
+        menuBar.add(next);
+        frame.getContentPane().add(splitPane);
         frame.setJMenuBar(menuBar);
         frame.setVisible(true); // Делаем окно видимым
 
+        //splitPane.setDividerLocation(0.5);
     }
 }
